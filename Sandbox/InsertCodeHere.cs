ede4989346538128ecc8cb9476ddd1b2eaff6edf
Lisa <Lisa@DESKTOP-TCI55VD>
2016-11-15 13:12:08 +0100

    Done!

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -10,28 +10,48 @@ namespace Sandbox
 
             // Create a new bank account with 25 % interest rate
             // (is that legal...?)
-            BankAccount theAccount = new BankAccount(25.0);
+            BankAccount theAccount;
 
-            theAccount.Deposit(2000);
-
-
-            // Should this be legal...?
-            theAccount.Deposit(-1000);
-
-            // Try to withdraw...
             try
             {
-                theAccount.Withdraw(3000);
+                theAccount = new BankAccount(15.0);
+            }
+            catch (IllegalInterestRateException)
+            {
+                Console.WriteLine("Illegal intest rate. The interest rate must be between 0.0 and 20.0 %");
+                return;
+            }
+            // Make a deposit
+
+            try
+            {
+                theAccount.Deposit(2000);
+            }
+            catch (NegativeAmountException)
+            {
+                Console.WriteLine("Illegal amount. Must be positive!");
+                return;
+            }
+
+
+
+            //    Should this be legal...?
+
+
+            //    Try to withdraw...
+            try
+            {
+                theAccount.Withdraw(500);
             }
             catch (WithdrawAmountTooLargeException)
             {
                 Console.WriteLine("You tried to withdraw too much money!");
+                return;
             }
 
-
             Console.WriteLine("Balance is now : " + theAccount.GetBalance());
-
-            // The LAST line of code should be ABOVE this line
-        }
+      
+    // The LAST line of code should be ABOVE this line
+}
     }
 }
